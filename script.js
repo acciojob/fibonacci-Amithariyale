0db8f3e03edf4f3fca3b104685f6f683620ed96f
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2023-12-08 17:58:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
 // your code here
+	if(num==1)return 0;
+	let first=0;
+	let second=1;
+	for(let i=2;i<num;i++){
+		const temp=second;
+		second=first+second;
+		first=temp;
+	}
+	return second;
 }
 
 module.exports = fibonacci;
